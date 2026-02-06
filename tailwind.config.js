3f19ae6269be2be05c2855ea28ead1fed20aa3a2
sinnoken@live.com <sinnoken@live.com>
2026-02-06 20:48:07 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
+        "./index.html",         // 確保有這行
         "./*.html",          // 掃描根目錄下的所有 HTML
         "./js/**/*.js",      // 如果你的 JS 裡有動態產生 class，也要掃描
         "./js/**/*.json",    // 掃描 JSON（如果你裡面有存 class 名稱）
