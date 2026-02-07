6087f8b46cfbfb6c2698a5ec9cf425b46c127b26
sinnoken@live.com <sinnoken@live.com>
2026-02-07 16:53:22 +0800

    update

--- js/vlan.js
+++ js/vlan.js
@@ -98,7 +98,7 @@ class VlanAuditor {
 
         try {
             console.log("🌐 Fetching OUI from CDN...");
-            const res = await fetch('https://cdn.jsdelivr.net/gh/silverwind/oui-data@master/index.json', {
+            const res = await fetch('js/silverwind.json', {
                 signal: controller.signal // 綁定超時訊號
             });
 
